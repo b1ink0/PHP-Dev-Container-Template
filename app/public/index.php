58bb5d533ca937901d47ebece4f24f2d87e4cde4
b1ink0 <loginfor357@gmail.com>
2023-09-23 10:00:51 +0530

    add: mysql setup added

--- app/public/index.php
+++ app/public/index.php
@@ -1,2 +1,10 @@
 <?php
+$pdo = new PDO('mysql:dbname=db;host=mysql', 'root_user', 'root', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+$query = $pdo->query('SHOW VARIABLES like "version"');
+
+$row = $query->fetch();
+
+echo "<h1>" . 'MySQL version:' . $row['Value'] . "</h1>";
+
 phpinfo();
