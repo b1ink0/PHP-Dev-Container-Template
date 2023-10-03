b6b6a8f2b334423a5c360fae7638df5c263fdf0c
b1ink0 <loginfor357@gmail.com>
2023-10-03 18:45:41 +0530

    add: composer setup for php

--- app/public/index.php
+++ app/public/index.php
@@ -1,2 +1,14 @@
 <?php
-phpinfo();
+require '../vendor/autoload.php';
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('../logs/your.log'));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
+
